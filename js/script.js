3a666832b6f6a9c9b98a9bee730e9214dd2f965b
Denis <denmarch816@gmail.com>
2018-09-22 00:37:01 +0400

    Исправление скрипта+замена фото

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var slides = document.querySelectorAll('.slider__show .slider__foto');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,3000);
+var slideInterval = setInterval(nextSlide,5000);
 
 function nextSlide() {
     slides[currentSlide].className = 'slider__foto';
     currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slider__foto-showing';
+    slides[currentSlide].className = 'slider__foto slider__foto-showing';
 }
